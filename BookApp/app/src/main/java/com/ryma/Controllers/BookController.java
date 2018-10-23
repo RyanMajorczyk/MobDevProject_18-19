818564b8bd3a4744f0b25c2daa9939573132a146
RyanMajorczyk <ryan.majorczyk@student.pxl.be>
2018-10-23 13:57:50 +0200

    Seperating controller classes

--- BookApp/app/src/main/java/com/ryma/Controllers/BookController.java
+++ BookApp/app/src/main/java/com/ryma/Controllers/BookController.java
@@ -1,23 +1,13 @@
-package com.ryma.bookapp;
+package com.ryma.Controllers;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-
-import com.ryma.Controllers.BookController;
+import com.ryma.bookapp.MainActivity;
+import com.ryma.bookapp.R;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -27,25 +17,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 import data.Book;
 
-public class MainActivity extends AppCompatActivity {
+public class BookController {
+    private Book bookToGiveBack = new Book();
+    private Activity activity;
 
-    public void sendMessage(View view) {
+    public Book getBookById(int id, MainActivity mainActivity) {
         BookController bookController = new BookController();
-        Book book = bookController.getBookById(1, this);
+        final String URL = "http://81.240.220.38:8090/book/" + id;
+        new BookController.RESTTask().execute(URL);
 
+        activity = mainActivity;
+
+        return bookToGiveBack;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-/*
     class RESTTask extends AsyncTask<String, Void, ResponseEntity<Book>> {
 
         //Implements method
@@ -70,13 +57,10 @@ public class MainActivity extends AppCompatActivity {
 
         protected void onPostExecute(ResponseEntity<Book> result) {
             HttpStatus status = result.getStatusCode();
-            Book book = result.getBody();
-            TextView t = (TextView) findViewById(R.id.textView);
-            t.setText(book.toString());
+            bookToGiveBack = result.getBody();
+            TextView t = (TextView) activity.findViewById(R.id.textView);
+            t.setText(bookToGiveBack.toString());
         }
 
     }
-*/
 }
-
-
