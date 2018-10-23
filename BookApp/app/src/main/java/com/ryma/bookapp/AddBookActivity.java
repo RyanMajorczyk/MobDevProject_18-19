3175d4c99d0e8a164a08560a8a6757ea92214446
maarten-vrijens <maarten.vrijens1@hotmail.com>
2018-10-23 13:57:34 +0200

    Edited layout

--- BookApp/app/src/main/java/com/ryma/bookapp/AddBookActivity.java
+++ BookApp/app/src/main/java/com/ryma/bookapp/AddBookActivity.java
@@ -3,7 +3,7 @@ package com.ryma.bookapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class activity_add_book extends AppCompatActivity {
+public class AddBookActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ BookApp/app/src/main/java/com/ryma/bookapp/AddBookFragment.java
@@ -0,0 +1,7 @@
+package com.ryma.bookapp;
+
+import android.support.v4.app.Fragment;
+
+public class AddBookFragment extends Fragment {
+
+}
