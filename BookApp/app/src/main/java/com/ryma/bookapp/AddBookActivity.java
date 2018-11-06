f3642bb5d54572afe1fed586971d077f1c775535
maarten-vrijens <maarten.vrijens1@hotmail.com>
2018-11-06 14:23:40 +0100

    Revert "edited fragments"
    This reverts commit 358babd6b1a7f2b5d53356f0f283d790495d73ca.

--- BookApp/app/src/main/java/com/ryma/bookapp/AddBookActivity.java
+++ BookApp/app/src/main/java/com/ryma/bookapp/AddBookActivity.java
@@ -9,7 +9,6 @@ import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -35,18 +34,35 @@ import java.io.FileOutputStream;
 import data.Book;
 import data.Review;
 
-public class AddBookActivity extends FragmentActivity {
+public class AddBookActivity extends AppCompatActivity {
     private static final int CAMERA_PIC_REQUEST = 1111;
 
     private ImageView mImage;
+
+    private EditText isbn;
+    private EditText title;
+    private EditText author;
+    private EditText description;
+
+    private byte[] image;
+
+    private Book bookToAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
 
-        mImage = findViewById(R.id.imageView_cover);
+        isbn = findViewById(R.id.editText_isbn);
+        title = findViewById(R.id.editText_title);
+        author = findViewById(R.id.editText_author);
+        description = findViewById(R.id.editText_description);
+
+        mImage = (ImageView) findViewById(R.id.imageView_cover);
     }
+
+
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
--- BookApp/app/src/main/java/com/ryma/bookapp/MenuActivity.java
+++ BookApp/app/src/main/java/com/ryma/bookapp/MenuActivity.java
@@ -25,7 +25,7 @@ public class MenuActivity extends AppCompatActivity
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-         toggle.syncState();
+        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
