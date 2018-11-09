b781c83018fa013048e1b88c47b85091d673dfc3
maarten-vrijens <maarten.vrijens1@hotmail.com>
2018-11-09 09:36:16 +0100

    Edited RecyclerViewAdapter

--- BookApp/app/src/main/java/com/ryma/bookapp/BookDetail/BookDetailFragment.java
+++ BookApp/app/src/main/java/com/ryma/bookapp/BookDetail/BookDetailFragment.java
@@ -2,23 +2,19 @@ package com.ryma.bookapp.BookDetail;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.ryma.bookapp.MainActivity;
 import com.ryma.bookapp.R;
 import com.ryma.bookapp.RecyclerViewAdapter;
-import com.ryma.bookapp.RecyclerViewFragment;
 import com.ryma.bookapp.RecyclerViewReviewAdapter;
 
 import org.springframework.http.HttpEntity;
--- BookApp/app/src/main/java/com/ryma/bookapp/RecyclerViewAdapter.java
+++ BookApp/app/src/main/java/com/ryma/bookapp/RecyclerViewAdapter.java
@@ -12,11 +12,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import domainModels.Book;
+
 //import domainModels.Book;
 
-public class RecyclerViewAdapter  {
-    /*
-    extends RecyclerView.Adapter<BookViewHolder>
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyBookViewHolder> {
+
+
     private Book[] books;
 
     private Context context;
@@ -29,37 +31,48 @@ public class RecyclerViewAdapter  {
 
     @NonNull
     @Override
-    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public MyBookViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(context).inflate(R.layout.recyclerview_books_list_item, viewGroup, false);
-        return new BookViewHolder(v);
+        return new MyBookViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BookViewHolder bookViewHolder, int position) {
+    public void onBindViewHolder(@NonNull MyBookViewHolder myBookViewHolder, int position) {
         Book book = books[position];
 
         try {
             if (book.getFront_cover() != null) {
                 Bitmap bitmap = BitmapFactory.decodeByteArray(book.getFront_cover(), 0, book.getFront_cover().length);
                 //bookViewHolder.mBookImage.setImageBitmap(bitmap);
-                bookViewHolder.mBookImage.setImageResource(R.mipmap.book_icon);
+                myBookViewHolder.mBookImage.setImageResource(R.mipmap.book_icon);
             } else {
-                bookViewHolder.mBookImage.setImageResource(R.mipmap.book_icon);
+                myBookViewHolder.mBookImage.setImageResource(R.mipmap.book_icon);
             }
         } catch (Exception ex) {
             Toast.makeText(context, "ImageConversion failed", Toast.LENGTH_SHORT).show();
         }
 
-
-        bookViewHolder.mTitleTextView.setText(book.getTitle());
-        bookViewHolder.mAuthorTextView.setText(book.getAuteur());
-
+        myBookViewHolder.mTitleTextView.setText(book.getTitle());
+        myBookViewHolder.mAuthorTextView.setText(book.getAuteur());
     }
 
     @Override
     public int getItemCount() {
         return books.length;
-    }*/
+    }
+
+    public class MyBookViewHolder extends RecyclerView.ViewHolder{
+        TextView mTitleTextView;
+        TextView mAuthorTextView;
+        ImageView mBookImage;
+
+        public MyBookViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTitleTextView = itemView.findViewById(R.id.textView_title);
+            mAuthorTextView = itemView.findViewById(R.id.textView_author);
+            mBookImage = itemView.findViewById(R.id.imageView_cover);
+        }
+    }
 }
 
 
